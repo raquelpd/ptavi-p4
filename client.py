81158e35948634fbd4c22351f7b24522dc519700
Raquel Perez Dominguez <raquelpd@gamma36.aulas.gsyc.urjc.es>
2016-10-17 10:38:24 +0200

    IP, Server y LINE pasados por el terminal. AP.4

--- client.py
+++ client.py
@@ -4,19 +4,21 @@
 Programa cliente UDP que abre un socket a un servidor
 """
 
-import socket
+import sys
+import socket      #lo utilizamos para abrir caminos del cliente al servidor
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+# Constantes. Dirección IP del servidor y contenido a enviar -(En mayúsculas)
+
+SERVER = sys.argv[1]
+PORT = int(sys.argv[2])
+LINE = sys.argv[3:]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket: #(red, paquete)
+    my_socket.connect((SERVER, PORT)) #Tupla (multiples valores que no se pueden modificar)
+    print("Enviando:", ' '.join(LINE))
+    my_socket.send(bytes(' '.join(LINE), 'utf-8') + b'\r\n')   #b: bytes
+    data = my_socket.recv(1024)    #1024: tamaño del bufer en bytes
+    print('Recibido -- ', data.decode('utf-8'))  #Decode: Pasar de bytes a utf8
 
 print("Socket terminado.")
