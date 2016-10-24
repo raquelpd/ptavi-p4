211b8d5036ea2c2b9408827fe45200f489e7db12
Raquel Perez Dominguez <raquelpd@gamma22.aulas.gsyc.urjc.es>
2016-10-24 16:03:19 +0200

    REGISTER

--- client.py
+++ client.py
@@ -10,15 +10,28 @@ import socket      #lo utilizamos para abrir caminos del cliente al servidor
 # Constantes. Dirección IP del servidor y contenido a enviar -(En mayúsculas)
 
 SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-LINE = sys.argv[3:]
+try:
+    PORT = int(sys.argv[2])
+except ValueError:
+    sys.exit("invalid Port")
+
+METODO = sys.argv[3].upper()
+USUARIO= sys.argv[4]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket: #(red, paquete)
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT)) #Tupla (multiples valores que no se pueden modificar)
-    print("Enviando:", ' '.join(LINE))
-    my_socket.send(bytes(' '.join(LINE), 'utf-8') + b'\r\n')   #b: bytes
-    data = my_socket.recv(1024)    #1024: tamaño del bufer en bytes
-    print('Recibido -- ', data.decode('utf-8'))  #Decode: Pasar de bytes a utf8
+if sys.argv[3] == 'REGISTER':
+    REGISTER  = ('REGISTER' + ' sip: ' + sys.argv[4] + ' SIP/2.0')
+    my_socket.send(bytes(REGISTER, 'utf-8') + b'\r\n\r\n')
+try:
+    data = my_socket.recv(1024)
+except ConnectionRefusedError:
+    sys.exit("Connection refused")
+    
+print('Recibido -- ', data.decode('utf-8'))  #Decode: Pasar de bytes a utf8
 
-print("Socket terminado.")
+my_socket.close()
+print('fin')
